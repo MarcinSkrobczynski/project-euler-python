fe9c0a6fbff296fcbeba9745b9929d22d2efc22b
Marcin Skrobczyński <m.skrobczynski@gmail.com>
2019-12-28 21:14:40 +0100

    P2: Add solution

---
+++ problem_0002.py
@@ -0,0 +1,19 @@
+# Solution to Project Euler Problem #2: Even Fibonacci numbers
+# Copyright (c) MarcinSkrobczynski
+
+
+def main(n: int):
+    sums = 0
+    old = curr = 1
+    while curr < n:
+        if curr % 2 == 0:
+            sums += curr
+
+        old, curr = curr, old + curr
+
+    return sums
+
+
+if __name__ == "__main__":
+    print(f"{main(100)} => {main(100) == 44}")  # 44 = 2 + 8 + 34
+    print(f"{main(4000000)}")
