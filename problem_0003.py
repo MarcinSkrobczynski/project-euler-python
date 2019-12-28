419939b8eb57c2433d1a4c09aeb06e400229c927
Marcin Skrobczyński <m.skrobczynski@gmail.com>
2019-12-28 22:06:06 +0100

    P3: Add solution

---
+++ problem_0003.py
@@ -0,0 +1,26 @@
+# Solution to Project Euler Problem #3: Largest prime factor
+# Copyright (c) MarcinSkrobczynski
+
+from math import sqrt
+
+
+def main(n: int):
+    max_prime = int(sqrt(n))
+    num = n
+    i = 0
+
+    while num % 2 == 0:
+        num //= 2
+
+    for i in range(3, max_prime, 2):
+        while num % i == 0:
+            num //= i
+        if num == 1:
+            break
+
+    return i
+
+
+if __name__ == "__main__":
+    print(f"{main(13195)} => {main(13195) == 29}")
+    print(f"{main(600851475143)}")
