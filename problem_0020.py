5ab1cce64c0b81b83d285c565f98144b8fe1aabf
Marcin Skrobczyński <m.skrobczynski@gmail.com>
2020-01-01 20:47:08 +0100

    P20: Add solution

---
+++ problem_0020.py
@@ -0,0 +1,13 @@
+# Solution to Project Euler Problem #20: Factorial digit sum
+# Copyright (c) MarcinSkrobczynski
+
+from math import factorial
+
+
+def main(n: int):
+    return sum(int(x) for x in str(n))
+
+
+if __name__ == "__main__":
+    print(f"{main(factorial(10))} => {main(factorial(10)) == 27}")
+    print(f"{main(factorial(100))}")
